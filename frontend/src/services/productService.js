5ade42eae3214af72369a37001e4c7a3ef4c0b9b
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-09-04 18:26:03 -0500

    Update productService.js

--- frontend/src/services/productService.js
+++ frontend/src/services/productService.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+export const getProducts = async () => {
+  return await axios.get(`${API_URL}/marketplace/`);
+};
+
+export const getProductById = async (id) => {
+  return await axios.get(`${API_URL}/marketplace/product/${id}`);
+};
+
+export const createProduct = async (product) => {
+  return await axios.post(`${API_URL}/marketplace/sell`, product);
+};
 
