8e84d3bf5acb4a9976bffb47988d63d49ed3268c
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-09-04 16:20:20 -0500

    Create product.py

---
+++ backend/models/product.py
@@ -0,0 +1,17 @@
+from . import db
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'price': self.price,
+            'owner': self.owner.serialize()
+        }
