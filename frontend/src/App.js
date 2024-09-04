f9d9eea05192ae73df010f856abf201d5c8bfbda
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-09-04 18:27:01 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+import ProductPage from './pages/ProductPage';
+import SellProduct from './components/SellProduct';
+import Login from './components/Login';
+import Register from './components/Register';
+import Profile from './components/Profile';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/product/:id" component={ProductPage} />
+        <Route path="/sell" component={SellProduct} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route path="/profile" component={Profile} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
 
