a650420872789915c8109b1df4aeac2345f4e9e3
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-09-04 18:26:27 -0500

    Update transactionService.js

--- frontend/src/services/transactionService.js
+++ frontend/src/services/transactionService.js
@@ -1,1 +1,8 @@
+import axios from 'axios';
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+export const purchaseProduct = async (productId) => {
+  return await axios.post(`${API_URL}/transaction/purchase`, { product_id: productId });
+};
 
