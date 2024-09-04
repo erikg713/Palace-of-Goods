b226990e7e0bbd5db2b60c36179bbb5bc805fd17
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-09-04 18:24:14 -0500

    Update HomePage.js

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import ProductList from '../components/ProductList';
+
+function HomePage() {
+  return (
+    <div className="container">
+      <h1>Welcome to Palace of Goods</h1>
+      <ProductList />
+    </div>
+  );
+}
+
+export default HomePage;
 
