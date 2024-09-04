415df90a326d54b03edb68404f11af328d949c44
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-09-04 18:24:45 -0500

    Update ProductPage.js

--- frontend/src/pages/ProductPage.js
+++ frontend/src/pages/ProductPage.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import ProductDetail from '../components/ProductDetail';
+
+function ProductPage() {
+  return (
+    <div className="container">
+      <ProductDetail />
+    </div>
+  );
+}
+
+export default ProductPage;
 
