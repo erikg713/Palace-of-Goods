fc6124860c51ca3e7812ecd6220f76edfcea6730
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-09-27 10:22:08 -0500

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'SC6ZJYWDGKLQ44SPQFK2SXCH2U3OVIFRWVXFOPRAVT3WAYFPQH2VQ3Z4')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PI_NETWORK_URL = os.getenv('PI_NETWORK_URL', 'https://sandbox.minepi.com/app/palace-of-goods')
