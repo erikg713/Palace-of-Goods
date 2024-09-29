16dc4c6ef51255cffa4c8f54676e496245f2fd44
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-09-29 09:14:13 -0500

    Update __init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,24 +1,15 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from config import Config
-from .routes import auth_bp, transactions_bp, products_bp, contracts_bp, error_handlers
-
-db = SQLAlchemy()
+from .config import Config
+from .routes import auth, products, transactions
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    # Initialize database
-    db.init_app(app)
-
     # Register Blueprints
-    app.register_blueprint(auth_bp)
-    app.register_blueprint(transactions_bp)
-    app.register_blueprint(products_bp)
-    app.register_blueprint(contracts_bp)
-
-    # Register error handlers
-    app.register_error_handler(Exception, error_handlers.handle_error)
+    app.register_blueprint(auth)
+    app.register_blueprint(products)
+    app.register_blueprint(transactions)
 
     return app
+
