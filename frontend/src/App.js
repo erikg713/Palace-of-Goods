515ccc038bdb75af03ba1648b87f36f2ec72e313
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-09-29 09:17:48 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,43 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import HomePage from './pages/HomePage';
-import ProductPage from './pages/ProductPage';
-import SellProduct from './components/SellProduct';
-import Login from './components/Login';
-import Register from './components/Register';
-import Profile from './components/Profile';
-// src/App.js
-import React from 'react';
-import RecursionLevel from './components/RecursionLevel';
-
-const App = () => {
-  return (
-    <div className="App">
-      <h1>Welcome to CodeQuest!</h1>
-      <RecursionLevel />
-      {/* Other components can be added here */}
-    </div>
-  );
-};
-
-export default App;
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route path="/" exact component={HomePage} />
-        <Route path="/product/:id" component={ProductPage} />
-        <Route path="/sell" component={SellProduct} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-        <Route path="/profile" component={Profile} />
-      </Switch>
-    </Router>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/login" component={Login} />
+                <Route path="/register" component={Register} />
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
+
 
