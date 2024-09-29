a96f2667e245e9ef8d969c27dcc4aa703ca94953
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-09-29 08:22:44 -0500

    Update routes

---
+++ backend/app/routes/__init__.py
@@ -0,0 +1,6 @@
+from flask import Blueprint
+
+auth_bp = Blueprint('auth', __name__)
+transactions_bp = Blueprint('transactions', __name__)
+products_bp = Blueprint('products', __name__)
+contracts_bp = Blueprint('contracts', __name__)
