011e92f2a318513e24e6ea67e146875cafc77935
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-09-29 08:30:21 -0500

    Update models

---
+++ backend/app/models/transaction.py
@@ -0,0 +1,11 @@
+from .. import db
+
+class Transaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    receiver_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    timestamp = db.Column(db.DateTime, default=db.func.current_timestamp())
+
+    sender = db.relationship('User', foreign_keys=[sender_id])
+    receiver = db.relationship('User', foreign_keys=[receiver_id])
