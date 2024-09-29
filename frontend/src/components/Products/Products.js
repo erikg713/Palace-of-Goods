fc2003f6bebc597ae4bc1df51af56444770fb160
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-09-29 08:45:01 -0500

    Update components

---
+++ frontend/src/components/Products/Products.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const Products = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const response = await axios.get('/api/products');
+            setProducts(response.data.products);
+        };
+        fetchProducts();
+    }, []);
+
+    return (
+        <div>
+            <h1>Products</h1>
+            <ul>
+                {products.map(product => (
+                    <li key={product.id}>{product.name} - ${product.price}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Products;
