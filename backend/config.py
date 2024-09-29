9911cd782bbedb678b7ed626deff9ff43f3fdf0a
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-09-29 06:04:55 -0500

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,17 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'mysecret')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://user:password@localhost/palaceofgoods')
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'jwtsecret')
-    PI_NETWORK_KEY = os.getenv('PI_NETWORK_KEY', 'my-pi-key')
-
-
-
-
-import os
-
-class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'SC6ZJYWDGKLQ44SPQFK2SXCH2U3OVIFRWVXFOPRAVT3WAYFPQH2VQ3Z4')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
