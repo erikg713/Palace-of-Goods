3b8097145d75f267cbe025edff8e1621f5c731d0
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-10-03 11:16:09 -0500

    Update services

---
+++ frontend/src/services/piNetwork.js
@@ -0,0 +1,11 @@
+// src/services/piNetwork.js
+
+import { Pi } from "pi-network-sdk";
+
+export const initializePiNetwork = () => {
+  Pi.init({
+    version: "2.0",
+    sandbox: process.env.REACT_APP_PI_NETWORK_SANDBOX === 'true',
+    appId: process.env.REACT_APP_PI_NETWORK_SDK_KEY
+  });
+};
