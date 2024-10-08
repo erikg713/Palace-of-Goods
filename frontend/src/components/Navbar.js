084964f1e44281118d26e429a09fd7e513c4f505
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-10-07 22:45:56 -0500

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function Navbar() {
-  return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/sell">Sell</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/register">Register</Link>
-      <Link to="/profile">Profile</Link>
-    </nav>
-  );
-}
+const Navbar = ({ isAuthenticated }) => {
+    return (
+        <nav>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/profile">Profile</Link></li>
+                <li><Link to="/cart">Cart</Link></li>
+                {isAuthenticated ? (
+                    <li><Link to="/logout">Logout</Link></li>
+                ) : (
+                    <li><Link to="/login">Login</Link></li>
+                )}
+            </ul>
+        </nav>
+    );
+};
 
 export default Navbar;
-
